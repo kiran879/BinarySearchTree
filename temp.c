87bf5d46e3d7b8a9090ee5d75f36654a099abf91
kiran879 <rajascricketer@gmail.com>
2025-06-06 16:10:58 +0100

    Temporary file

---
+++ temp.c
@@ -0,0 +1,16 @@
+#include"headers.h"
+#include"dataStructures.h"
+#include"declarations.h"
+
+void** (void **arg)
+{
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+
+
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+
+}

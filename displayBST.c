2cc5cf26c589614fb5b617dc4f1e86b8d76e3898
kiran879 <rajascricketer@gmail.com>
2025-06-06 16:03:54 +0100

    Initial commit: Used to display the Binary serach tree

---
+++ displayBST.c
@@ -0,0 +1,24 @@
+#include"headers.h"
+#include"dataStructures.h"
+#include"declarations.h"
+
+void* displayBST(void *arg)
+{
+	Node *parent;
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+	parent=(Node*)arg;
+
+	if(*(parent->child+0))
+		displayBST((void*)*(parent->child+0));//passing left child to the display function
+	printf("%d->",parent->data);//printing the current node's data(which will be the parent for every display call)
+	if(*(parent->child+1))
+		displayBST((void*)*(parent->child+1));//passing right child to the display function
+
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+	return 0;
+
+}

cebc0b911eab22805ae2d2b541d3e71305c05310
kiran879 <rajascricketer@gmail.com>
2025-06-06 16:03:27 +0100

    Initial commit: Used to create new node

---
+++ createNode.c
@@ -0,0 +1,35 @@
+#include"headers.h"
+#include"dataStructures.h"
+#include"declarations.h"
+
+void* createNode(void *arg)
+{
+	Node *new;
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+	new=(Node*)malloc(sizeof(Node));
+	if(!new)
+	{
+		perror("malloc");
+		(*fptr[1])((void*)"failure");
+	}
+	new->child=(Node**)malloc(sizeof(Node*)*2);
+	if(!new->child)
+	{
+		perror("malloc");
+		(*fptr[1])((void*)"failure");
+	}
+	memset(new->child,'\0',sizeof(Node*)*2);
+//	*(new->child+0)=(Node*)malloc(sizeof(Node));
+//	*(new->child+1)=(Node*)malloc(sizeof(Node));
+//	*(new->child+0)=NULL;
+//	*(new->child+1)=NULL;
+	printf("FILE:%s -> %s:Please enter data for the new node\n",__FILE__,__func__);
+	scanf("%d",&new->data);
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Ends\n",__FILE__,__func__);
+#endif
+	return (void*)new;
+
+}

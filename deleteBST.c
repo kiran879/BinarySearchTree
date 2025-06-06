01a7b35fcedff17f1bdd8545b7be6dce84c70411
kiran879 <rajascricketer@gmail.com>
2025-06-06 16:08:13 +0100

    Initial commit: delete node and tree functions, just mapped as of now

---
+++ deleteBST.c
@@ -0,0 +1,17 @@
+#include"headers.h"
+#include"dataStructures.h"
+#include"declarations.h"
+
+void* deleteBST(void *arg)
+{
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+
+
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Ends\n",__FILE__,__func__);
+#endif
+	return 0;
+
+}
---
+++ deleteNode.c
@@ -0,0 +1,17 @@
+#include"headers.h"
+#include"dataStructures.h"
+#include"declarations.h"
+
+void* deleteNode(void *arg)
+{
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+
+
+#ifdef DEBUG
+	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
+#endif
+	return 0;
+
+}

b2e1804af75e5feab8b6c2bb19f66da32c5cbbee
kiran879 <rajascricketer@gmail.com>
2025-06-07 20:47:42 +0100

    Typo

--- displayBST.c
+++ displayBST.c
@@ -5,9 +5,11 @@
 void* displayBST(void *arg)
 {
 	Node *parent;
+	/*
 #ifdef DEBUG
 	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
 #endif
+*/
 	parent=(Node*)arg;
 
 	if(*(parent->child+0))
@@ -15,10 +17,11 @@ void* displayBST(void *arg)
 	printf("%d->",parent->data);//printing the current node's data(which will be the parent for every display call)
 	if(*(parent->child+1))
 		displayBST((void*)*(parent->child+1));//passing right child to the display function
-
+/*
 #ifdef DEBUG
 	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
 #endif
+*/
 	return 0;
 
 }

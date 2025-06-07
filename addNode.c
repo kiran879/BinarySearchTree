1470c1e9c1112ba3d5ee39028798e3c430174e6f
kiran879 <rajascricketer@gmail.com>
2025-06-07 20:47:18 +0100

    Updated! add new lines of code to correct the logic

--- addNode.c
+++ addNode.c
@@ -5,33 +5,42 @@
 void* addNode(void *arg)
 {
 	Tree *ltree;
-	Node *new,*cur;
+	Node *new;
+	Node *cur;
+	Node *temp;//to keep track of last visited node
 #ifdef DEBUG
 	printf("FILE:%s -> %s:Begins\n",__FILE__,__func__);
 #endif
 	ltree=(Tree*)arg;
 	new=(Node*)(*fptr[7])(0);//createNode
+	if(*new->child+1)
+	{
+		printf("FILE:%s -> %s:Inside if \n",__FILE__,__func__);
+
+	}
 	if(ltree->root==NULL)
 	{
 		ltree->root=new;
 		return (void*)ltree;
 	}
-	cur=ltree->root;
+	temp=cur=ltree->root;
 	while(cur)
-	{
-		
-		
-		printf("FILE:%s -> %s:Testing inside while %d , new data:%d\n",__FILE__,__func__,cur->data,new->data);
+	{	
+		temp=cur;//temp stores the cur node before it gets updated in the below lines
+		//printf("FILE:%s -> %s:Testing inside while %d , new data:%d\n",__FILE__,__func__,cur->data,new->data);
 		if(new->data <= cur->data)
 			cur= *(cur->child+0);
 		else
 			cur= *(cur->child+1);
-		printf("FILE:%s -> %s:Testing end of while size of cur:%ld\n",__FILE__,__func__,sizeof(cur));
+		//printf("FILE:%s -> %s:Testing end of while size of cur:%ld\n",__FILE__,__func__,sizeof(cur));
 	}
-	cur=new;
-	printf("FILE:%s -> %s:Testing outside while %d\n",__FILE__,__func__,cur->data);
-
-
+	if(new->data <= temp->data)
+		*(temp->child+0)= new;
+	else
+		*(temp->child+1)= new;
+	
+	//cur=new;
+	//printf("FILE:%s -> %s:Testing outside while %d\n",__FILE__,__func__,cur->data);
 #ifdef DEBUG
 	printf("FILE:%s -> %s:Ends\n",__FILE__,__func__);
 #endif
--- createNode.c
+++ createNode.c
@@ -20,11 +20,9 @@ void* createNode(void *arg)
 		perror("malloc");
 		(*fptr[1])((void*)"failure");
 	}
-	memset(new->child,'\0',sizeof(Node*)*2);
-//	*(new->child+0)=(Node*)malloc(sizeof(Node));
-//	*(new->child+1)=(Node*)malloc(sizeof(Node));
-//	*(new->child+0)=NULL;
-//	*(new->child+1)=NULL;
+//	memset(new->child,'\0',sizeof(Node*)*2);
+	*(new->child+0)=NULL;
+	*(new->child+1)=NULL;
 	printf("FILE:%s -> %s:Please enter data for the new node\n",__FILE__,__func__);
 	scanf("%d",&new->data);
 #ifdef DEBUG
